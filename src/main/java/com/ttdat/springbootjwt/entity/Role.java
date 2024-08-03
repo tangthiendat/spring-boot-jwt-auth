ea8883c066544f411627d6d5efa173c256f94151
tangthiendat <thiendattang@gmail.com>
2024-08-03 14:34:13 +0700

    Set up project for JWT Authentication

---
+++ src/main/java/com/ttdat/springbootjwt/entity/Role.java
@@ -0,0 +1,5 @@
+package com.ttdat.springbootjwt.entity;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/com/ttdat/springbootjwt/entity/User.java
@@ -0,0 +1,43 @@
+package com.ttdat.springbootjwt.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    Integer id;
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}
